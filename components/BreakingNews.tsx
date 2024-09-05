2ad0749f57fa33b8f90b450eb07133fd8200272f
mohammed jamal <yamni.zero@gmail.com>
2024-09-05 12:01:35 +0200

    - Animated card -

--- components/BreakingNews.tsx
+++ components/BreakingNews.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import { Colors } from '@/constants/Colors'
 import { NewsDataType } from '@/types'
 import SliderItem from '@/components/SliderItem'
-import Animated, { useAnimatedRef, useSharedValue } from 'react-native-reanimated'
+import Animated, { useAnimatedRef, useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated'
 
 type Props = {
     newList: Array<NewsDataType>
@@ -15,6 +15,12 @@ const BreakingNews = ({ newList }: Props) => {
     const scrollX = useSharedValue(0);
     const ref = useAnimatedRef<Animated.FlatList<any>>();
 
+    const onScrollhandler = useAnimatedScrollHandler({
+        onScroll: (e) => {
+            scrollX.value = e.contentOffset.x;
+        },
+    })
+
 
     return (
         <View style={styles.continer}>
@@ -25,11 +31,13 @@ const BreakingNews = ({ newList }: Props) => {
                     data={data}
                     keyExtractor={(_, index) => `list_item${index}`} 
                     renderItem={({ item, index }) => (
-                        <SliderItem slideItem={item} index={index} />
+                        <SliderItem slideItem={item} index={index} scrollX={scrollX} />
                     )}
                     horizontal
                     showsHorizontalScrollIndicator={false}
                     pagingEnabled
+                    onScroll={onScrollhandler}
+                    scrollEventThrottle={16}
                     />
             </View>
         </View>
--- components/SliderItem.tsx
+++ components/SliderItem.tsx
@@ -1,22 +1,64 @@
 import { Dimensions, Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { NewsDataType } from '@/types'
+import { Extrapolation, interpolate, SharedValue, useAnimatedStyle } from 'react-native-reanimated'
+import { LinearGradient } from 'expo-linear-gradient'
+import { Colors } from '@/constants/Colors'
+import Animated from 'react-native-reanimated';
 
 type Props = {
     slideItem: NewsDataType,
-    index: number
+    index: number,
+    scrollX: SharedValue<number>
 }
 
 const { width } = Dimensions.get('screen');
 
-const SliderItem = ({ slideItem, index }: Props) => {
+const SliderItem = ({ slideItem, index, scrollX }: Props) => {
+
+    const rnStyle = useAnimatedStyle(() => {
+        return {
+            transform: [
+                {
+                    translateX: interpolate(
+                        scrollX.value,
+                        [(index - 1) * width, index * width, (index + 1) * width],
+                        [-width * 0.15, 0, width * 0.15],
+                        Extrapolation.CLAMP
+                    ),
+                },
+                {
+                    scale: interpolate(
+                        scrollX.value,
+
+                        [(index - 1) * width, index * width, (index + 1) * width],
+                        [0.9, 1, 0.9],
+                        Extrapolation.CLAMP
+
+                    ),
+                },
+            ],
+        };
+    })
+
     return (
-        <View style={styles.itemWrapper}>
+        <Animated.View style={[styles.itemWrapper, rnStyle]} key={slideItem.article_id}>
             <Image source={{ uri: slideItem.image_url }} style={styles.image} />
-            <Text>
-                {slideItem.title}
-            </Text>
-        </View>
+            <LinearGradient colors={["transparent", 'rgba(0,0,0,0.8']} style={styles.background}>
+                <View style={styles.sourceInfo}>
+
+                    {slideItem.source_icon && (
+                        <Image source={{ uri: slideItem.source_icon }} style={styles.sourceIcon} />
+                    )}
+                    <Text style={styles.sourceName}>{slideItem.source_name}</Text>
+                </View>
+                <Text style={styles.title} numberOfLines={2}>
+                    {slideItem.title}
+                </Text>
+
+            </LinearGradient>
+
+        </Animated.View>
     )
 }
 
@@ -30,9 +72,49 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     image: {
-        width: width,
+        width: width - 60,
         height: 180,
         borderRadius: 20
+    },
+    background: {
+        position: 'absolute',
+        left: 30,
+        right: 0,
+        top: 0,
+        width: width - 60,
+        height: 180,
+        borderRadius: 20,
+        padding: 20,
 
     },
+    sourceInfo: {
+        flexDirection: 'row',
+        position: 'absolute',
+        top: 85,
+        paddingHorizontal: 20,
+        alignItems: 'center',
+        gap: 10,
+    },
+    sourceName: {
+        color: Colors.white,
+        fontSize: 12,
+        fontWeight: '600',
+
+    },
+    sourceIcon: {
+        width: 25,
+        height: 25,
+        borderRadius: 20,
+    },
+    title: {
+        color: Colors.white,
+        fontSize: 14,
+        position: 'absolute',
+        top: 120,
+        paddingHorizontal: 20,
+        fontWeight: '600',
+
+    }
+
+
 })
